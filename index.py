880aacb3b12dcde46c41bbb39c3fbb7ad126fc6c
Reuben <Earthbaker@users.noreply.github.com>
2018-01-22 00:14:48 +1000

    Added env.json and planet_gen.py

--- index.py
+++ index.py
@@ -1,14 +1,15 @@
 import planet_main as pm
 import facebook, requests
 import json
+import sys
 
-env = json.loads(open('env.json').read())
-page_id = '140719353303841'
+env = json.loads(open(sys.path[0] + '\env.json').read())
+page_id = env['page_id']
 at = env['page_token']
 
 graph = facebook.GraphAPI(access_token=at)
 
-pm.make_img(5)
+pm.make_img(7)
 
 graph.put_photo(image=open('movie.gif', 'rb'), message='')
 
